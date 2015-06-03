ba4cf032aa151da4c35d5081c2996ced02b5f9b0
Tom Canac <contact@tomcanac.com>
2015-06-03 19:59:47 -0400

    all sheets are nextified now

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 
 // Include plugins
+var minifycss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
 var selector = require('postcss-custom-selectors');
 var calc = require("postcss-calc");
@@ -20,7 +22,12 @@ gulp.task('styles-next', function () {
     ];
     gulp.src("next/knacss.css")
         .pipe(postcss(processors))
+        .pipe(sourcemaps.init())
         .pipe(rename('knacss-unminified.css'))
+        .pipe(gulp.dest('./css'))
+        .pipe(rename('knacss.css'))
+        .pipe(minifycss({keepBreaks:false,keepSpecialComments:0}))
+        .pipe(sourcemaps.write('.', {includeContent: false}))
         .pipe(gulp.dest('./css'));
 });
 
